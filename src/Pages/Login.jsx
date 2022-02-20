61c01554b624cf876b1a46d0304057fe142b0f35
Leo Clarke <leojclarke@gmail.com>
2022-02-20 20:00:28 +0100

    Add Login components

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,41 @@
+import { Avatar, Button, Container, TextField } from "@mui/material";
 import React from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "70vh",
+        gap: 4,
+      }}
+    >
+      <Avatar>M</Avatar>
+      <form>
+        <TextField
+          fullWidth
+          placeholder="username"
+          label="username"
+          name="username"
+          variant="outlined"
+          type="text"
+        />
+        <TextField
+          fullWidth
+          placeholder="password"
+          label="password"
+          name="password"
+          variant="outlined"
+          type="password"
+        />
+        <Button variant="contained" type="submit">
+          LOGIN
+        </Button>
+      </form>
+    </Container>
+  );
 }
