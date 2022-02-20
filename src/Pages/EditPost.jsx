34598ec8305eac3e62fcc3e494f24d75bec6204b
Leo Clarke <leojclarke@gmail.com>
2022-02-20 20:57:13 +0100

    Add components to Edit Page

--- src/Pages/EditPost.jsx
+++ src/Pages/EditPost.jsx
@@ -1,5 +1,39 @@
+import { Button, Container, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
 
 export default function EditPost() {
-  return <div>EditPost</div>;
+  return (
+    <Container maxWidth="sm" sx={{ pt: "6rem" }}>
+      <Typography variant="h4" gutterBottom>
+        EDIT POST
+      </Typography>
+      <form onSubmit={(e) => console.log(e.target)}>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 4 }}>
+          <TextField
+            fullWidth
+            placeholder="title"
+            label="title"
+            name="title"
+            variant="outlined"
+            type="text"
+          />
+          <TextField
+            fullWidth
+            placeholder="body"
+            label="body"
+            name="body"
+            variant="outlined"
+            type="text"
+            multiline
+            maxRows="3"
+            minRows="3"
+          />
+          <Button variant="contained" type="submit">
+            SAVE POST
+          </Button>
+        </Box>
+      </form>
+    </Container>
+  );
 }
