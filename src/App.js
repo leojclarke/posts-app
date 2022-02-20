394a63f617e4e42c28ad6ca8028788d9b1f993ea
Leo Clarke <leojclarke@gmail.com>
2022-02-20 12:42:21 +0100

    Set up routing to a posts page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Posts from "./Pages/Posts";
 
 function App() {
-  return <div className="App">Posts-App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/posts" element={<Posts />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
